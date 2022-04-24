58b208416909e6ea6eb05481a82d27d82b19d928
Stefan <stefan.heil@stud-mail.uni-wuerzburg.de>
2022-04-24 21:11:05 +0200

    Adds first draft

--- capsule.py
+++ capsule.py
@@ -3,115 +3,101 @@ from torch import nn
 from torch.nn import functional as func
 
 
-"""
-TODO:
--> modellation decision: implicit or explicit modellation of the capsules
--> batched dynamic agreement routing --> the routing can be arbitrary for elements of the batch
+class Capsule(nn.Module):
 
---> these are some rather unpleasant concepts
+    def __init__(self,
+                 n_in_caps: int,
+                 in_caps_size: int,
+                 n_out_caps: int,
+                 out_caps_size: int,
+                 agreement_iterations: int = 5):
 
-Planning:
+        """
+            Parameters:
+            -----------
 
-Let B the batch, N the input capsules, Cin the in capsule size, Cout the out capsule size, M the number of output capsules
+            n_in_caps: int
+                Number of input capsules. Referred to in comments as 'N'
+            in_caps_size: int
+                Number of neurons per input capsule. Referred to in comments as 'Cin'
+            n_out_caps: int
+                Number of output capsules. Referred to in comments as 'M'
+            out_caps_size: int
+                Number of neurons per output capsule. Referred to in comments as 'Cout'
+            agreement_iterations: int
+                number of iterations of the dynamic agreement routing algorithm
+        """
+        super().__init__()
 
-Input: (B,N,Cin)
+        self.transforms = nn.Parameter(torch.rand((n_out_caps,
+                                                   n_in_caps,
+                                                   in_caps_size,
+                                                   out_caps_size)))  # (M, N, Cin, Cout)
 
-For the calculation of the output capsule activations
-Assignment weights: (B,M,N) --> expanded to (B, M, N, 1) (broadcastable)
-Expansion of the input: (B,1,N,Cin)
-Elementwise multiplication yields: (B, M, N, Cin)
-Contraction along the penultimate axis: (B, M, Cin) --> yields the consensus for each output capsule
+        self.n_in_caps = n_in_caps
+        self.in_caps_size = in_caps_size
+        self.n_out_caps = n_out_caps
+        self.out_caps_size = out_caps_size
+        self.agreement_iterations = agreement_iterations
 
-#Dot product calculation
+    def _forward(self, expanded_x, weights):
 
-Tranpose input: (B,Cin,N)
--> Calculate the matrix product with (B, M, Cin) ---> yields (B, M, N): the raw new weights
--> Calculate the softmax normalization along PENULTIMATE axis --> (B, M, N)
+        weights = torch.unsqueeze(weights, dim=-1)  # (B, M, N, 1)
+        weighted_caps = expanded_x * weights  # (B, M, N, Cin)
 
----> iterate: fix number of dimensions or until the change in each batch dimension is below a threshold?
+        expanded_weighted_caps = torch.unsqueeze(weighted_caps, dim=3)  # (B, M, N, 1, Cin)
+        transformed_x = torch.squeeze(expanded_weighted_caps @ self.transforms, dim=3)  # (B, M, N, Cout)
 
---> Cout does not play any role here (obviously)
+        caps_consensus = torch.sum(transformed_x, dim=2)  # (B, M, Cout)
+        return transformed_x, caps_consensus
 
-"""
+    def _dynamic_agreement_routing(self, x):
 
-# a raw implementation:
+        # Constants
+        expanded_x = torch.unsqueeze(x, dim=1)  # (B, 1, N, Cin)
 
-def dynamic_agreement_routing(input : torch.tensor,
-                              n_output_capsules,
-                              max_iterations: int = 5,
-                              convergence_threshold: float = 0.1):
+        # initialize assignment-logits
+        weights = (1 / self.n_in_caps) * torch.ones((x.shape[0], self.n_out_caps, self.n_in_caps))  # (B, M, N)
 
-    # Constants
-    transposed_input = torch.transpose(input, dim0=2, dim1=1)  # B x Cin x N
-    expanded_input = torch.unsqueeze(input, dim=1)  #B x 1 x N x Cin
+        for i in range(self.agreement_iterations):
+            transformed_x, caps_consensus = self._forward(expanded_x, weights)  # (B, M, N, Cout), (B, M, Cout)
+            caps_consensus = torch.unsqueeze(caps_consensus, dim=-1)  # (B, M, Cout, 1)
 
-    #initialize assignment-logis
-    weights = (1/input.shape[1]) * torch.ones((input.shape[0], n_output_capsules, input.shape[1]))
-    prev_weights = torch.zeros((input.shape[0], n_output_capsules, input.shape[1]))
+            caps_assignment_logits = torch.squeeze(transformed_x @ caps_consensus, dim=-1)  #(B, M, N)
+            weights = func.softmax(caps_assignment_logits, dim=1)  # (B, M, N)
 
-    for i in range(max_iterations):
+        return weights  # (B, M, N)
 
-        weights = torch.unsqueeze(weights, dim=-1)  # yields B x M x N x 1
+    def _squash(self, x):
+        """
+            Calculates the squash activation function for Capsule layers
 
-        weighted_capsules = expanded_input * weights
-        capsule_consensus_vector = torch.sum(weighted_capsules, dim=2)  # yields B x M x Cin
+            Parameters
+            ----------
+            x: torch.tensor
+                tensor of shape (B, M, Cout), where B is the batch size
+        """
+        lengths = torch.sqrt(torch.sum(torch.square(x), dim=-1, keepdim=True))
+        unit_vectors = x / lengths
 
-        capsule_assignment_logits = capsule_consensus_vector @ transposed_input  # yields B x M x N
+        scale = lengths / (lengths + 1)
+        return scale * unit_vectors
 
-        weights = func.softmax(capsule_assignment_logits, dim=1)
+    def forward(self, x):
+        """
+            Calculates the forward pass of Capsule Networks
 
-        # early termination criteria
-        diff = torch.sum(torch.abs(prev_weights - weights) / (weights + 1e-10)) / (input.shape[0] * input.shape[1])
-        if diff < 1:  # that needs to be fixed
-            break
+            Parameters
+            ---------
+            x: torch.tensor
+                Torch tensor of shape (B, N, Cin), where B is the batch
+        """
+        weights = self._dynamic_agreement_routing(x)  # (B, M, N)
+        expanded_x = torch.unsqueeze(x, dim=1)  # (B, 1, N, Cin)
 
-    # exiting the loop, weights will have the correct shape (B, M, N)
-    return weights
+        _, res = self._forward(expanded_x, weights)  # _, (B, M, Cout)
 
-a = dynamic_agreement_routing(torch.ones(10, 9, 8), 5, max_iterations=100)
-
-
-"""
- Regarding the forward pass:
- 
- expanded weights: B x M x N x 1
- 
- expanded input : B x 1 x N x Cin
- 
- --> elementwise multiplication yiels: B x M x N x Cin --> the weighted input capsules for every output capsule
- 
-----> Forward propagation:
-
---> reshape: B x M x N x Cin --->  B x M x (NxCin) x 1
---> weight matrix is M x Cout x (NxCin)
-
---> matrix multiplication M x Cout x (NxCin) @ B x M x (NxCin) x 1 ----> yields B x M x Cout x 1
- 
-"""
-
-def forward(input, n_output_capsules, output_capsule_size):
-
-    weights = dynamic_agreement_routing(input, n_output_capsules, max_iterations=10)
-
-    weights = torch.unsqueeze(weights, dim=-1)  #alright we expand again after all
-    expanded_input = expanded_input = torch.unsqueeze(input, dim=1)  #B x 1 x N x Cin
-
-    weighted_input = weights * expanded_input
-
-    weighted_input = torch.reshape(weighted_input, shape=(input.shape[0], n_output_capsules, -1, 1))
-
-    # some default value:
-    weight_tensor = torch.ones((n_output_capsules, output_capsule_size, weighted_input.shape[2]))
-
-    res = torch.squeeze(weight_tensor @ weighted_input, dim=-1)
-
-    print(res.shape)  # B x n_output_capsules x output_capsule_size
-
-forward(torch.ones(10, 9, 8), 5, 4)
-
-
-# 1.25h of work ---> acceptable...
-
+        return self._squash(res)
 
 
 
